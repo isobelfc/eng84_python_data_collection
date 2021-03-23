acab537857a86baadf164014f8370b2b0f19ea3a
Isobel Fitt-Conway <isobelfc@gmail.com>
2021-03-23 12:30:23 +0000

    added list information and examples

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Data Collections in Python
+- Lists and Tuples
+- Dictionaries
+- Sets
+
+### What is a list?
+- Lists are commonly used to store data
+- They are mutable
+- Syntax `[]` used to create a list
+- List items are also indexed
+- Different data types can be mixed in the same list
+  
+### List methods
+- List values can be changed with eg. `ex_list[0] = "first"` to change the first value to "first"
+- `ex_list.append("new")` adds the value "new" to the end of the list
+- `ex_list.pop(0)` removes the value at the given index, or the last value if no index is specified
+- `ex_list.remove("item")` removes the value "item" from the list
+- We can also use slices with lists
---
+++ lists&tuples.py
@@ -0,0 +1,29 @@
+# What is a list?
+# Lists are commonly used to store data
+# Lists are MUTABLE
+# Syntax [] used to create a list
+
+shopping_list = ["bread", "chocolate", "avocados", "milk"]
+# print(shopping_list)
+# print(type(shopping_list))
+# print(shopping_list[1])
+# print(shopping_list[-2])
+
+# Change the value of 0 index from bread to oranges
+# shopping_list[0] = "oranges"
+# print(shopping_list)
+
+# Add a value to the list
+# shopping_list.append("ice cream")
+# print(shopping_list)
+
+# Remove a value from the list
+shopping_list.pop(0)
+print(shopping_list)
+
+# We can mix data types in a list
+mixed_list = [1, 2, 3, "one", "two", "three"]
+print(mixed_list)
+
+# Slices with lists
+print(mixed_list[2:3])
